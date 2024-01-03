6c21ebd99f4dacf82759beb1c3f97193696872a0
lukas <lukasurna@gmail.com>
2024-01-03 14:43:48 +0200

    alien, ship collision.

--- settings.py
+++ settings.py
@@ -8,12 +8,13 @@ class Settings:
         self.screen_height = 800
         self.bg_color = (10, 6, 6)
 
-        #Ship speed
+        #Ship settings
         self.ship_speed = 1.5
+        self.ship_limit = 3
 
         #bullet settings
-        self.bullet_speed = 2.0
-        self.bullet_width = 3
+        self.bullet_speed = 2.5
+        self.bullet_width = 300
         self.bullet_height = 15
         self.bullet_color = (60, 60, 60)
         self.bullets_allowed = 3
